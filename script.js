07a57710f64a8ef3b0da04da221d028a303c7d7f
ozgeerkskn <ozgeerkskn@hotmail.com>
2023-01-31 12:41:07 +0300

    CinemaBookingApp
    You can choose movies and seats through the cinema booking application.

---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const amount = document.getElementById("amount");
+const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
+
+getFromLocalStorage();
+CalculateTotal();
+container.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("reserved")
+  ) {
+    e.target.classList.toggle("selected");
+    CalculateTotal();
+  }
+});
+select.addEventListener("change", function (e) {
+  CalculateTotal();
+});
+
+function CalculateTotal() {
+  const selectedSeats = container.querySelectorAll(".seat.selected");
+
+  const selectedSeatsArr = [];
+  const seatsArr = [];
+
+  selectedSeats.forEach(function (seat) {
+    selectedSeatsArr.push(seat);
+  });
+
+  seats.forEach(function (seat) {
+    seatsArr.push(seat);
+  });
+
+  let selectedSeatIndexs = selectedSeatsArr.map(function (seat) {
+    return seatsArr.indexOf(seat);
+  });
+
+  console.log(selectedSeatIndexs);
+
+  let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
+  count.innerText = selectedSeatCount;
+
+  amount.innerText = selectedSeatCount * select.value;
+
+  saveToLocalStorage(selectedSeatIndexs);
+}
+
+function getFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach(function (seat, index) {
+      if (selectedSeats.indexOf(index) > -1) seat.classList.add("selected");
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
+
+function saveToLocalStorage(index) {
+  localStorage.setItem("selectedSeats", JSON.stringify(index));
+  localStorage.setItem("selectedMovieIndex", select.selectedIndex);
+}
